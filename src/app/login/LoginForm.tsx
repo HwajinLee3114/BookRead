69b32992cd75e574b210b7223100f2a6d7bbe496
hjlee3114 <hwajin3114@gmail.com>
2024-10-06 17:30:15 +0900

    유효성 공통화 로그인 페이지 적용

--- src/app/login/LoginForm.tsx
+++ src/app/login/LoginForm.tsx
@@ -1,27 +1,25 @@
 "use client"; // 클라이언트 컴포넌트로 설정
 
 import React, { useState } from "react";
-import { validateInput } from "@/utils/comn";
+import { validateField } from "@/utils/comn";
+import InputField from "@/components/InputField";
 
 const LoginForm: React.FC = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [emailError, setEmailError] = useState<string>("");
-//   const [passwordError, setPasswordError] = useState<string>("");
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
 
     if (name === "email") {
       setEmail(value);
-      const { isValid, errorMessage } = validateInput(name, value);
-      setEmailError(isValid ? "" : errorMessage || '');
+      const { isValid, errorMessage } = validateField(name, value);
+      setEmailError(isValid ? "" : errorMessage || "");
     }
 
     if (name === "password") {
       setPassword(value);
-    //   const { isValid, errorMessage } = validateInput(name, value);
-    //   setPasswordError(isValid ? "" : errorMessage || '');
     }
   };
 
@@ -37,39 +35,28 @@ const LoginForm: React.FC = () => {
 
   return (
     <div className="g_form_container mgt_1r">
-      <label className="g_label" htmlFor="email">이메일</label>
-      <input
+      <InputField
+        label="이메일"
         type="email"
         value={email}
         name="email"
-        onChange={handleChange}
         placeholder="이메일을 입력하세요"
-        required
-        className="g_input"
+        onChange={handleChange}
+        errorMessage={emailError}
       />
-      {emailError && (
-        <p className="g_invalid">유효한 이메일 주소를 입력해 주세요.</p>
-      )}
 
-      <label htmlFor="password" className="g_label mgt_1r">
-        비밀번호
-      </label>
-      <input
+      <InputField
+        label="비밀번호"
         type="password"
         value={password}
         name="password"
-        onChange={handleChange}
         placeholder="비밀번호를 입력하세요"
-        required
-        className="g_input"
+        onChange={handleChange}
       />
+
       <p className="g_invalid">로그인 정보가 올바르지 않습니다.</p>
 
-      <button
-        type="button"
-        onClick={handleLogin}
-        className="g_btn mgt_1r"
-      >
+      <button type="button" onClick={handleLogin} className="g_btn mgt_1r">
         로그인
       </button>
     </div>
